1b7ef525e5da1824ff039ead5286c5ded0c90c90
Ondrej Prazak <oprazak@redhat.com>
2015-02-16 12:29:46 +0100

    transactional jenkins instance creation

--- app/controllers/integration/api/jenkins_instances_controller.rb
+++ app/controllers/integration/api/jenkins_instances_controller.rb
@@ -1,3 +1,4 @@
+require 'uri'
 module Integration
   class Api::JenkinsInstancesController < Katello::Api::V2::ApiController
     respond_to :json
@@ -18,55 +19,68 @@ module Integration
       }
 
       respond_for_index(:collection => item_search(JenkinsInstance, params, options))
-   end
+    end
 
-   def create
-     @jenkins_instance = JenkinsInstance.new(jenkins_instance_params)
-     @jenkins_instance.organization = @organization     
-     @jenkins_instance.save!
-     task = sync_task(::Actions::Integration::JenkinsInstance::CreateJenkinsInstanceKeys, 
+    def create
+      @jenkins_instance = JenkinsInstance.new(jenkins_instance_params)
+      @jenkins_instance.organization = @organization          
+
+      rollback = false
+      JenkinsInstance.transaction do
+        @jenkins_instance.save!
+        task = sync_task(::Actions::Integration::JenkinsInstance::CreateJenkinsInstanceKeys, 
                       :url => jenkins_instance_params[:url],
-                      :passwd => "changeme",
+                      :cert_path => @jenkins_instance.cert_path,
                       :jenkins_home => jenkins_instance_params[:jenkins_home])
 
-     @jenkins_instance.pubkey = task.output.fetch(:pubkey)
-     @jenkins_instance.save!
-     respond_for_show(:resource => @jenkins_instance)
-   end
+        @jenkins_instance.pubkey = task.output.fetch(:pubkey)
+        @jenkins_instance.save!
 
-   def update
-     @jenkins_instance.update_attributes!(jenkins_instance_params)
-     @jenkins_instance.save!
+        if task.output.fetch(:status) == 1 
+          raise ActiveRecord::Rollback
+          rollback = true
+        end
+      end       
+      
+      if rollback
+        fail ::Katello::HttpErrors::Conflict, "Could not access Jenkins server, are you sure you set up certificates?"         
+      else
+        respond_for_show(:resource => @jenkins_instance)          
+      end     
+    end
 
-     respond_for_show(:resource => @jenkins_instance)
-   end
+    def update
+      @jenkins_instance.update_attributes!(jenkins_instance_params)
+      @jenkins_instance.save!
 
-   def show
-     respond_for_show(:resource => @jenkins_instance)
-   end
+      respond_for_show(:resource => @jenkins_instance)
+    end
 
-   def destroy
-     @jenkins_instance.destroy
-     respond_for_show(:resource => @jenkins_instance)
-   end
+    def show
+      respond_for_show(:resource => @jenkins_instance)
+    end
 
-   def check_jenkins
-     data = @jenkins_instance.check_jenkins_server
-     @jenkins_instance.server_version = data
-     respond_for_show
-   end
+    def destroy
+      @jenkins_instance.destroy
+      respond_for_show(:resource => @jenkins_instance)
+    end
 
-   protected
+    def check_jenkins
+      data = @jenkins_instance.check_jenkins_server
+      @jenkins_instance.server_version = data
+      respond_for_show
+    end
 
-   def find_jenkins_instance
-     @jenkins_instance = JenkinsInstance.find_by_id(params[:id])
-     fail HttpErrors::NotFound, "Could not find Jenkins Instance with id: #{params[:id]}" if @jenkins_instance.nil?
-     @jenkins_instance
-   end
+    protected
 
-   def jenkins_instance_params
-     params.require(:jenkins_instance).permit(:name, :url, :jenkins_home, :cert_path)
-   end
+    def find_jenkins_instance
+      @jenkins_instance = JenkinsInstance.find_by_id(params[:id])
+      fail ::Katello::HttpErrors::NotFound, "Could not find Jenkins Instance with id: #{params[:id]}" if @jenkins_instance.nil?
+      @jenkins_instance
+    end
 
- end
+    def jenkins_instance_params
+      params.require(:jenkins_instance).permit(:name, :url, :jenkins_home, :cert_path)
+    end
+  end
 end
--- app/lib/actions/integration/jenkins_instance/create_jenkins_instance_keys.rb
+++ app/lib/actions/integration/jenkins_instance/create_jenkins_instance_keys.rb
@@ -9,28 +9,39 @@ module Actions
         include Mixins::SshExtension
 
         def run
-          output[:pubkey] = fetch_pubkey(parse_host, input[:passwd])
+          output[:pubkey] = fetch_pubkey(parse_host)
         end
 
         def parse_host
           URI(input.fetch(:url)).host
         end
 
-        def fetch_pubkey(host, passwd)
-          buffer = StringIO.new          
+        def fetch_pubkey(host)
+          buffer = StringIO.new    
           ip = Socket::getaddrinfo(host, 'www', nil, Socket::SOCK_STREAM)[0][3]
-          Net::SSH.start(ip, 'root', :password => passwd) do |ssh|
+
+          Net::SSH.start(ip, 'root', :keys => [input.fetch(:cert_path)]) do |ssh|
             status = ssh_exec!(ssh, "ls #{key_location}")[2]           
-            ssh.exec! "mkdir #{key_location}" if status > 0
+            ssh.exec!("mkdir #{key_location}") if status > 0
             ssh.exec! "sed -i s:Defaults\\.*requiretty:#Defaults\\ requiretty:g /etc/sudoers"
+            ssh.exec! "rm -f #{key_location}/#{parse_host}.pub #{key_location}/#{parse_host}"
             ssh.exec! "sudo -u jenkins ssh-keygen -f #{key_location}/#{parse_host} -t rsa -N '' -q"
             ssh.scp.download! "#{key_location}/#{parse_host}.pub", buffer
-          end          
+          end  
+
           buffer.string
         end        
 
         def key_location
           "#{input.fetch(:jenkins_home)}/.ssh"
+        end
+
+        def finalize
+          if output[:pubkey].match(/^ssh-rsa/)
+            output[:status] = 0
+          else
+            output[:status] = 1
+          end
         end
 
       end
