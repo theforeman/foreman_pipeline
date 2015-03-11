8fe2e36400c8f18c4441ea87783a8bc274f3b94c
Ondrej Prazak <oprazak@redhat.com>
2015-03-11 18:09:42 +0100

    one task for cv+env in promotion hook

--- app/lib/actions/integration/job/cv_promote_job_hook.rb
+++ app/lib/actions/integration/job/cv_promote_job_hook.rb
@@ -8,23 +8,27 @@ module Actions
         end
 
         def plan(version, environment, is_force = false)          
-          
-
           valid_jobs = version.content_view.jobs.select { |job| job.is_valid? }
-          jobs_to_run = valid_jobs.select { |job| version.eql? job.target_cv_version }        
-          jobs_to_run.each do |job|
-            
-            if job.levelup_trigger && !job.version_already_promoted?
-              plan_self(:trigger => trigger.output, :job_id => job.id)    
-            end
-
+          jobs_to_run = valid_jobs.select { |job| version.eql? job.target_cv_version }
+          allowed_jobs = jobs_to_run.select { |job| job.levelup_trigger && !job.version_already_promoted? }
           
-          end
+          plan_self(:trigger => trigger.output,
+                    :job_ids => allowed_jobs.map(&:id),
+                    :job_names => allowed_jobs.map(&:name))    
         end
 
         def run
-          job = ::Integration::Job.find input[:job_id]
-          ForemanTasks.trigger(DeployNewHost, job)
+          fail "Multiple jobs defined for the same content view and environment: #{input[:job_names]}.
+               This may result in unexpected behaviour.
+               Resolve the conflict and rerun the task." if input[:job_ids].length > 1
+          if input[:job_ids].length == 1
+            job = ::Integration::Job.find(input[:job_ids].first)
+            ForemanTasks.trigger(DeployNewHost, job)
+          end
+        end
+
+        def rescue_strategy_for_self
+          Dynflow::Action::Rescue::Skip
         end
         
       end
--- app/lib/actions/integration/job/deploy_new_host.rb
+++ app/lib/actions/integration/job/deploy_new_host.rb
@@ -6,37 +6,37 @@ module Actions
 
         def plan(job)          
           sequence do
-            redeploy = plan_action(Redeploy, job)    
+            # redeploy = plan_action(Redeploy, job)    
                 
-            # h = { :host => {
-            #         :id => "random_number",
-            #         :name => "dummy host",
-            #         :ip => "192.168.100.103",
-            #         :mac => "no_mac_here",
-            #         :params => ["host_params_empty"]
-            #       },
-            #       :activation_key => {
-            #           :cp_id => 544553785444,
-            #       },
-            #       :packages => ["cat", "rat", "bat"]
-            # }
+            h = { :host => {
+                    :id => "random_number",
+                    :name => "dummy host",
+                    :ip => "192.168.100.103",
+                    :mac => "no_mac_here",
+                    :params => ["host_params_empty"]
+                  },
+                  :activation_key => {
+                      :cp_id => 544553785444,
+                  },
+                  :packages => ["cat", "rat", "bat"]
+            }
 
-            plan_action(Jenkins::WaitHostReady, :host_ip => redeploy.output[:host][:ip],
-                                                :jenkins_instance_hostname => jenkins_hostname(job),
-                                                :jenkins_home => job.jenkins_instance.jenkins_home,
-                                                :cert_path => job.jenkins_instance.cert_path)
+            # plan_action(Jenkins::WaitHostReady, :host_ip => redeploy.output[:host][:ip],
+            #                                     :jenkins_instance_hostname => jenkins_hostname(job),
+            #                                     :jenkins_home => job.jenkins_instance.jenkins_home,
+            #                                     :cert_path => job.jenkins_instance.cert_path)
             
-            plan_action(JenkinsInstance::Keyscan, :cert_path => job.jenkins_instance.cert_path,
-                                                  :jenkins_url => job.jenkins_instance.url,
-                                                  :jenkins_home => job.jenkins_instance.jenkins_home,
-                                                  :host_ip => redeploy.output[:host][:ip])            
+            # plan_action(JenkinsInstance::Keyscan, :cert_path => job.jenkins_instance.cert_path,
+            #                                       :jenkins_url => job.jenkins_instance.url,
+            #                                       :jenkins_home => job.jenkins_instance.jenkins_home,
+            #                                       :host_ip => redeploy.output[:host][:ip])            
 
-            packages = plan_action(FindPackagesToInstall, :job_id => job.id)
+            # packages = plan_action(FindPackagesToInstall, :job_id => job.id)
             ::User.current = ::User.anonymous_admin if ::User.current.nil?
             bulk_build = plan_action(Jenkins::BulkBuild, 
                                       job.jenkins_projects,
                                       :job_id => job.id,
-                                      :data => {:packages => packages.output[:package_names]}.merge(redeploy.output))
+                                      :data => h)#{:packages => packages.output[:package_names]}.merge(redeploy.output))
             plan_action(Promote, :job_id => job.id, :build_fails => bulk_build.output[:failed_count])
             
           end
