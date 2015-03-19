67ecb7d2872f27802a62e4f90b191aad28427518
Ondrej Prazak <oprazak@redhat.com>
2015-03-19 15:21:23 +0100

    cleanup

--- app/lib/actions/foreman_pipeline/job/deploy_new_host.rb
+++ app/lib/actions/foreman_pipeline/job/deploy_new_host.rb
@@ -14,25 +14,6 @@ module Actions
                                                 :jenkins_home => job.jenkins_instance.jenkins_home,
                                                 :cert_path => job.jenkins_instance.cert_path)
             
-            # plan_action(JenkinsInstance::Keyscan, :cert_path => job.jenkins_instance.cert_path,
-            #                                       :jenkins_url => job.jenkins_instance.url,
-            #                                       :jenkins_home => job.jenkins_instance.jenkins_home,
-            #                                       :host_ip => redeploy.output[:host][:ip])
-
-
-            # h = { :host => {
-            #         :id => "random_number",
-            #         :name => "dummy host",
-            #         :ip => "192.168.100.103",
-            #         :mac => " 01:23:45:67:89:ab",
-            #         :params => ["host_params_empty"]
-            #       },
-            #       :activation_key => {
-            #           :cp_id => 544553785444,
-            #       },
-                  
-            # }
-
             packages = plan_action(FindPackagesToInstall, :job_id => job.id)
 
 
