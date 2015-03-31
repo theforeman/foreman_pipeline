1b80535ce3e9a3820c03cbbcd050296003c65f42
Ondrej Prazak <oprazak@redhat.com>
2015-03-31 11:24:39 +0200

    fixed spinner for paths

--- app/models/foreman_pipeline/concerns/kt_environment_extension.rb
+++ app/models/foreman_pipeline/concerns/kt_environment_extension.rb
@@ -7,11 +7,6 @@ module ForemanPipeline
         has_many :job_paths, :class_name => 'ForemanPipeline::JobPath', :dependent => :destroy, :foreign_key => :path_id
         has_many :jobs, :through => :job_paths, :class_name => 'ForemanPipeline::Job'
       end
-
-      def full_paths
-        return [self.full_path] unless self.library?
-        self.organization.promotion_paths
-      end
       
     end
   end
