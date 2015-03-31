886d70907c40244225a6bbe572be18931cecc5ce
Ondrej Prazak <oprazak@redhat.com>
2015-03-31 11:07:01 +0200

    debug info

--- app/lib/actions/foreman_pipeline/job/deploy_new_host.rb
+++ app/lib/actions/foreman_pipeline/job/deploy_new_host.rb
@@ -7,19 +7,31 @@ module Actions
 
         def plan(job)          
           sequence do
-            redeploy = plan_action(Redeploy, job)    
+            # redeploy = plan_action(Redeploy, job)    
                 
-            plan_action(Jenkins::WaitHostReady, :host_ip => redeploy.output[:host][:ip],
-                                                :jenkins_instance_hostname => jenkins_hostname(job),
-                                                :jenkins_home => job.jenkins_instance.jenkins_home,
-                                                :cert_path => job.jenkins_instance.cert_path)
+            # plan_action(Jenkins::WaitHostReady, :host_ip => redeploy.output[:host][:ip],
+            #                                     :jenkins_instance_hostname => jenkins_hostname(job),
+            #                                     :jenkins_home => job.jenkins_instance.jenkins_home,
+            #                                     :cert_path => job.jenkins_instance.cert_path)
+            
+            h = { :host => {
+                    :id => "random_number",
+                    :name => "dummy host",
+                    :ip => "192.168.100.103",
+                    :mac => " 01:23:45:67:89:ab",
+                    :params => ["host_params_empty"]
+                  },
+                  :activation_key => {
+                      :cp_id => 544553785444,
+                  },                  
+            }
 
             packages = plan_action(FindPackagesToInstall, :job_id => job.id)
 
             bulk_build = plan_action(Jenkins::BulkBuild, 
                                       job.jenkins_projects,
                                       :job_id => job.id,
-                                      :data => redeploy.output,
+                                      :data => h,#redeploy.output,
                                       :packages => packages.output[:package_names])
             plan_action(Promote, :job_id => job.id, :build_fails => bulk_build.output[:failed_count])            
           end
