b8f94e82ee086fa6b900eafe423e06ead045a6f6
Ondrej Prazak <oprazak@redhat.com>
2016-05-18 14:28:01 +0200

    Make plugin tests run for PRs

--- test/unit/sanity_test.rb
+++ test/unit/sanity_test.rb
@@ -1,12 +1,7 @@
 require 'foreman_pipeline_plugin_test_helper'
 
 class IntegrationTest < ActiveSupport::TestCase
-  # setup do
-  #   User.current = User.admin
-  # end
-
   test "the truth" do
     assert true
   end
-
 end
