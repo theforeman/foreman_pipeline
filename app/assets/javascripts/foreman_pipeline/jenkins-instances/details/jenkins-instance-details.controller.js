cfb2f254eae645b51c0575b14729a7287c32db59
Ondrej Prazak <oprazak@redhat.com>
2016-06-29 16:35:55 +0200

    Fixes #15304 - Move reachability button to details-info

--- app/assets/javascripts/foreman_pipeline/jenkins-instances/details/jenkins-instance-details.controller.js
+++ app/assets/javascripts/foreman_pipeline/jenkins-instances/details/jenkins-instance-details.controller.js
@@ -1,4 +1,4 @@
-angular.module('ForemanPipeline.jenkins-instances').controller('JenkinsInstanceDetailsController', 
+angular.module('ForemanPipeline.jenkins-instances').controller('JenkinsInstanceDetailsController',
     ['$scope', '$state', 'JenkinsInstance', '$q', 'translate',
     function ($scope, $state, JenkinsInstance, $q, translate) {
 
@@ -14,27 +14,5 @@ angular.module('ForemanPipeline.jenkins-instances').controller('JenkinsInstanceD
         $scope.jenkinsInstance = JenkinsInstance.get({id: $scope.$stateParams.jenkinsInstanceId}, function () {
             $scope.panel.loading = false;
         });
-
-        $scope.checkJenkins = function () {
-            var deferred = $q.defer();
-            $scope.working = true;
-            JenkinsInstance.checkJenkins({id: $scope.jenkinsInstance.id}, function (response) {
-                deferred.resolve(response);                
-                $scope.successMessages.push(translate('Jenkins is reachable, server ver. %s').replace('%s', response.server_version));
-                $scope.$broadcast('successMessages', $scope.successMessages)
-                $scope.working = false;
-            }, function (response) {
-                deferred.reject(response);
-                angular.forEach(response.data.errors, function (errorMessage, key) {
-                        if (angular.isString(key)) {
-                            errorMessage = [key, errorMessage].join(' ');
-                        }
-                        $scope.errorMessages.push(translate("Could not reach Jenkins server: ") + errorMessage);
-                        $scope.broadcast('errorMessages', $scope.errorMessages);
-                    });
-                $scope.working = false;
-            })
-            return deferred.promise;
-        };
     }]
 );
