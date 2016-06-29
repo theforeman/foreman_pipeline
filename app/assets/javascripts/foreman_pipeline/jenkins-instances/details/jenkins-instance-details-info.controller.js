1ae5953cb01fe71edd65bf2992b4d7755b6572b5
Ondrej Prazak <oprazak@redhat.com>
2016-06-29 16:35:55 +0200

    Fixes #15304 - Move reachability button to details-info

--- app/assets/javascripts/foreman_pipeline/jenkins-instances/details/jenkins-instance-details-info.controller.js
+++ app/assets/javascripts/foreman_pipeline/jenkins-instances/details/jenkins-instance-details-info.controller.js
@@ -1,4 +1,4 @@
-angular.module('ForemanPipeline.jenkins-instances').controller('JenkinsInstanceDetailsInfoController', 
+angular.module('ForemanPipeline.jenkins-instances').controller('JenkinsInstanceDetailsInfoController',
     ['$scope', '$q', 'translate', 'JenkinsInstance',
     function ($scope, $q, translate, JenkinsInstance) {
 
@@ -41,6 +41,27 @@ angular.module('ForemanPipeline.jenkins-instances').controller('JenkinsInstanceD
         $scope.$on('errorMessages', function (event, data) {
             $scope.errorMessages = data;
         });
-        
+
+        $scope.checkJenkins = function () {
+            var deferred = $q.defer();
+            $scope.working = true;
+            JenkinsInstance.checkJenkins({id: $scope.jenkinsInstance.id}, function (response) {
+                deferred.resolve(response);
+                $scope.successMessages.push(translate('Jenkins is reachable, server ver. %s').replace('%s', response.server_version));
+                $scope.$broadcast('successMessages', $scope.successMessages)
+                $scope.working = false;
+            }, function (response) {
+                deferred.reject(response);
+                angular.forEach(response.data.errors, function (errorMessage, key) {
+                        if (angular.isString(key)) {
+                            errorMessage = [key, errorMessage].join(' ');
+                        }
+                        $scope.errorMessages.push(translate("Could not reach Jenkins server: ") + errorMessage);
+                        $scope.broadcast('errorMessages', $scope.errorMessages);
+                    });
+                $scope.working = false;
+            })
+            return deferred.promise;
+        };
     }]
 );
